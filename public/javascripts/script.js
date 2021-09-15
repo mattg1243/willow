e4182ad7baba34eb48a6acf496e18511d18b79a3
mattg1243 <mattgallucci97@gmail.com>
2021-09-15 14:44:14 -0700

    added dollar signs to currency values, changed some styling

---
+++ public/javascripts/script.js
@@ -0,0 +1,6 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });*/
+
+  M.AutoInit();
