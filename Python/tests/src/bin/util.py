4fd5f005ee2b038201039b2d0e3f8acaffbf5862
ethan <gallucci311@gmail.com>
2021-10-02 19:59:11 -0700

    try this on heroku

--- Python/tests/src/bin/util.py
+++ Python/tests/src/bin/util.py
@@ -17,7 +17,7 @@ from borb.pdf.canvas.layout.image.image import Image
 from borb.pdf.canvas.color.color import HexColor, X11Color
 from borb.pdf.canvas.layout.table.table import TableCell
 # b64
-import base64
+import binascii
 
 # Defines Mongo Cluster
 def _mongo_cluster():
@@ -315,6 +315,15 @@ def generate(CLIENT, DATES, TYPES, DURATIONS, RATES, AMOUNTS, BALANCE):
     page_layout.add(_build_statment_header())
     page_layout.add(_build_billing_table(cli))
     page_layout.add(_description_table(TYPES, DATES, DURATIONS, RATES, AMOUNTS, BALANCE))
-    
-    encoded_pdf = str(pdf)
-    return encoded_pdf
+
+
+    # Testing: write pdf file before binary conversion
+    #with open('../../binary_invoice_test/bintest.pdf', 'wb') as pdf_file:
+        #PDF.dumps(pdf_file, pdf)
+
+
+    str_pdf = str(pdf)
+   # bin_pdf = ''.join(format(ord(i), '08b' ) for i in str_pdf)
+
+    with open(f'public/invoices/{cli}.pdf', 'wb') as pdf_file:
+        PDF.dumps(pdf_file, pdf)
