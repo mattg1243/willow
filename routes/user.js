f34eda1ef9c0b704d22fb7473b72d44e95f3f67c
ethgallucci <gallucci311@gmail.com>
2021-12-02 23:54:54 -0800

    local testing paths && tidy up depracated

--- routes/user.js
+++ routes/user.js
@@ -307,13 +307,13 @@ router.get('/client/:id/makestatement/download/:clientname/:start/:end', functio
         'Location': "/users/dashboard"
     });
 
-    res.download(`/app/public/invoices/${req.params.clientname}.pdf`, `${req.params.clientname} ${req.params.start}-${req.params.end}.pdf`, function (err) {
+    res.download(`public/invoices/${req.params.clientname}.pdf`, `${req.params.clientname} ${req.params.start}-${req.params.end}.pdf`, function (err) {
 
         if (err) return console.error(err);
 
         
         // delete the pdf from the server after download
-        fs.unlink(`/app/public/invoices/${req.params.clientname}.pdf`, function (err) {
+        fs.unlink(`public/invoices/${req.params.clientname}.pdf`, function (err) {
             
             if (err) return console.error(err)
 
