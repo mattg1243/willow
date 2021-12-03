b7a56d95bfdc429f26d9be33b9163f158002684d
ethgallucci <gallucci311@gmail.com>
2021-12-02 23:25:33 -0800

    generator v0.0.8

--- Python/tests/src/bin/main.py
+++ Python/tests/src/bin/main.py
@@ -1,50 +1,50 @@
-import sys
-import datetime
-from bson import ObjectId
-from util import _set_crit, _mongo_cluster, _mongo_client, _record_handling
+from util import generate
 from logger import _set_log_params
+from digest import get_from, get_to, load, header_grab, parse_dates, parse_types, parse_durations
+from digest import parse_rates, parse_amounts, parse_balances
 
 # Defines logging params
-logger = _set_log_params()
+#logger = _set_log_params()
 
 # Main Script
 if __name__ == "__main__":
-    # Target Client
     try:
-        CLIENT_ID = ObjectId(sys.argv[1])
-    # Target Client Error Handling
-    except Exception as CLIENT_ID_error:
-        print("Invalid client ID syntax: %s" % CLIENT_ID_error)
-    # Passed Date Range to query Mongo
+        # Statement start date
+        FROM = get_from()
+        # Statement end date
+        TO = get_to()
+        # User info
+        header = header_grab()
+        # Event log
+        data = load()
+        # Parse dates
+        dates = parse_dates(data)
+        # Parse types
+        types = parse_types(data)
+        # Parse durations
+        durations = parse_durations(data)
+        # Parse rates
+        rates = parse_rates(data)
+        # Parse amounts
+        amounts = parse_amounts(data)
+        # Parse new balances
+        newBalances = parse_balances(data)
+        
+        print('DATES -> ', dates)
+        print('TYPES -> ', types)
+        print('DURATIONS -> ', durations)
+        print('RATES -> ', rates)
+        print('AMOUNTS -> ', amounts)
+        print('NEWBALANCES -> ', newBalances)
+        print('START -> ', FROM, 'END -> ', TO)
+        print('USER INFO -> ', header)
+        
+    except Exception as e:
+        print("Error parsing arguments: %s", e)
+        
     try:
-        FROM = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d")
-        TO = datetime.datetime.strptime(sys.argv[3], "%Y-%m-%d")
-        CLIENT_NAME = str(sys.argv[4])
-    # Date Range and client name Error Handling
-    except Exception as date_error_handle:
-        print("Error parsing date range: %s" % date_error_handle)
-    # Target Critical
-    critical = _set_crit(CLIENT_ID, FROM, TO)
-    # Make Mongo Connection
-    try:
-        # Initialize Cluster
-        cluster = _mongo_cluster()
-        client = _mongo_client(cluster)
-        # Set Instance for statements
-        # events
-        db = client.maindb
-        events_col = db.events
-        # Access Collection and Pull All Crtical Data
-        data_fetched = events_col.find(critical).sort("date", 1)
-    # Mongo Connection Error Handling
-    except Exception as _mongo_error_handler:
-        print("Error connecting to MongoDB: %s" % _mongo_error_handler)
-
-    # Handles Records Obtained
-    try:
-        _record_handling(data_fetched, CLIENT_ID, CLIENT_NAME)
-
-    # Record Handle Error Handling
-    except Exception as _generator_error_handler:
-        # print('Error generating statements: %s' % _generator_error_handler)
-        logger.critical("Error generating statements: %s" % _generator_error_handler)
+        generate(header['clientname'], dates, types, durations, rates, amounts, newBalances)
+    
+    except Exception as e:
+        print('Error generating statement: %s', e)
+        
