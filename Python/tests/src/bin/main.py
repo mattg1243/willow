2d78a070b1a5ffeaf09b8a282c58786a710855a7
ethgallucci <gallucci311@gmail.com>
2021-12-11 15:23:08 -0800

    tidy up - documentation for statement functions

--- Python/tests/src/bin/main.py
+++ Python/tests/src/bin/main.py
@@ -21,28 +21,23 @@ if __name__ == "__main__":
     data = load()
     # Parse dates
     dates = parse_dates(data)
-    print('DATES -> ', dates)
     # Parse types
     types = parse_types(data)
-    print('TYPES -> ', types)
     # Parse durations
     durations = parse_durations(data)
-    print('DURATIONS -> ', durations)
     # Parse rates
     rates = parse_rates(data)
-    print('RATES -> ', rates)
     # Parse amounts
     amounts = parse_amounts(data)
-    print('AMOUNTS -> ', amounts)
     # Parse new balances
     newBalances = parse_balances(data)
-    print('NEWBALANCES -> ', newBalances)
     # Find running balance
     running_balance = find_running_balance(newBalances)
     print('RUNNING BALANCE -> ', running_balance)
     
     # Check if statment is single paged or multipaged
     multipage = check_multipage(dates)
-    # Generat the statement
+    
+    # Generate the statement
     generate_statement(header['clientname'], dates, types, durations, rates, amounts, newBalances, multipage)
   
