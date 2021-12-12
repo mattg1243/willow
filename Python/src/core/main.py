b19ff81a3a8b2ffcbda98e315d19a3b843a1237d
ethgallucci <gallucci311@gmail.com>
2021-12-11 22:37:05 -0800

    impl logger

--- Python/src/core/main.py
+++ Python/src/core/main.py
@@ -10,37 +10,50 @@ logger = _set_log_params()
 # Main Script
 if __name__ == "__main__":
     
-    # Statement start date
-    FROM = get_from()
-    # Statement end date
-    TO = get_to()
-    print('START -> ', FROM, 'END -> ', TO)
-    # User info
-    header = header_grab()
-    print('USER INFO -> ', header)
-    # Event log
-    data = load()
-    # Parse dates
-    dates = parse_dates(data)
-    # Parse types
-    types = parse_types(data)
-    # Parse durations
-    durations = parse_durations(data)
-    # Parse rates
-    rates = parse_rates(data)
-    # Parse amounts
-    amounts = parse_amounts(data)
-    # Parse new balances
-    newBalances = parse_balances(data)
-    # Find running balance
-    running_balance = find_running_balance(newBalances)
-    print('RUNNING BALANCE -> ', running_balance)
+    # Parse Arguments
+    try:
+        # Statement start date
+        FROM = get_from()
+        # Statement end date
+        TO = get_to()
+        print('START -> ', FROM, 'END -> ', TO)
+        # User info
+        header = header_grab()
+        print('USER INFO -> ', header)
+        # Event log
+        data = load()
+        # Parse dates
+        dates = parse_dates(data)
+        # Parse types
+        types = parse_types(data)
+        # Parse durations
+        durations = parse_durations(data)
+        # Parse rates
+        rates = parse_rates(data)
+        # Parse amounts
+        amounts = parse_amounts(data)
+        # Parse new balances
+        newBalances = parse_balances(data)
+        # Find running balance
+        running_balance = find_running_balance(newBalances)
+        print('RUNNING BALANCE -> ', running_balance)
+    
+    # If Argument Parsing Failed - Log & Terminate     
+    except Exception as ArgParseError:
+        logger.critical("Failed Parsing Arguments!: %s" % ArgParseError)
+        exit(0)
     
     # Check if statment is single paged or multipaged
     multipage = check_multipage(dates)
     
     # Generate the statement
-    generate_statement(header['clientname'], dates, types, durations, rates, amounts, newBalances, multipage)
+    try:
+        generate_statement(header['clientname'], dates, types, durations, rates, amounts, newBalances, multipage)
+   
+    # Log Error & Terminate
+    except Exception as StatementError:
+        logger.critical("Failed to Generate Statement!: %s" % StatementError)
+        exit(0)
     
-    # Generate QR Code facing Willow home page for now...
+    # Generate QR Code if Script was Successful - facing Willow home page for now...
     generate_qr()
