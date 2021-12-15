21ddf63d965a8fd5ebed145a8ca0da15bcd7237e
mattg1243 <mattgallucci97@gmail.com>
2021-12-14 23:08:08 -0800

    all routes and handlers have now been seperated

--- routes/client.js
+++ routes/client.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const connectEnsureLogin = require('connect-ensure-login');
 const handlers = require('./handlers/clientHandlers');
 
+
+// render the client page and get all events
 router.get("/:id", connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.renderClientPage(req, res) });
+// add new event to clients record
+router.post('/:id/addsession', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.addEvent(req, res); });
+// get a specific event and render the event page
+router.get('/event/:eventid', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.renderEventPage(req, res); });
+// update an event
+router.post('/event/:eventid', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.updateEvent(req, res); });
+// delete an event
+router.get('/deleteevent/:id/:eventid', connectEnsureLogin.ensureLoggedIn(), function (req, res) { handlers.deleteEvent(req, res) });
+// generate a statment
+router.post('/:id/makestatement/:fname/:lname', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.makeStatement(req, res); })
+// download the statement
+router.get('/:id/makestatement/download/:clientname/:start/:end', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.downloadStatement(req, res); })
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,187 +1,23 @@
-var router = require('express').Router();
-var Client = require('../models/client-schema')
-var Event = require('../models/event-schema')
-var connectEnsureLogin = require('connect-ensure-login');
-var fs = require('fs');
-let {PythonShell} = require('python-shell')
-const helpers = require('./helpers/helpers')
+const router = require('express').Router();
+const Client = require('../models/client-schema')
+const Event = require('../models/event-schema')
+const connectEnsureLogin = require('connect-ensure-login');
 const handlers = require('./handlers/userHandlers');
 
-
-router.get('/register', function(req, res, next) {
-  
-    res.render('register');
-
-})
-
-router.get('/logout', function(req, res) { req.logout(); res.redirect('/login'); })
-
+// render register page
+router.get('/register', (req, res) => { res.render('register'); })
+// logout user
+router.get('/logout', (req, res) => { req.logout(); res.redirect('/login'); })
 // register new user to DB
 router.post('/register/newuser', (req, res) => { handlers.registerUser(req, res) })
 // render logged in user's dashboard page
 router.get('/dashboard', connectEnsureLogin.ensureLoggedIn('/login'), (req, res) => { handlers.renderDashboard(req, res) })
 // render user preference page
-router.get('/preferences', function (req, res) { res.render('preferences', { user: req.user }); })
+router.get('/preferences', (req, res) => { res.render('preferences', { user: req.user }); })
 // update users info from preferences page
-router.post("/update-info/:id", (req, res) => { handlers.updateUserInfo(req, res) })
+router.post("/update-info/:id", connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.updateUserInfo(req, res) })
 // add a new client
 router.post('/dashboard/newclient', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.addNewClient(req, res) });
 
-router.post('/client/:id/addsession', connectEnsureLogin.ensureLoggedIn(), function(req, res){
-
-    let time = parseFloat(req.body.hours) + parseFloat(req.body.minutes)
-    let amount = 0;
-    
-    if (req.body.type == 'Retainer') { // need to do something similar for refund type
-
-        amount = req.body.amount;
-
-        } else if(req.body.type == 'Refund') {
-
-            amount = -(req.body.amount);
-
-        } else {
-
-        amount = -(time * req.body.rate)
-    
-     }
-
-    Client.findOne({ _id: req.params.id }, function(err, client) {
-
-        if (err) return console.error(err);
-
-        const event = new Event({ clientID: req.params.id, date: req.body.date, type: req.body.type, detail: req.body.detail, duration: time, rate: req.body.rate, amount: parseFloat(amount).toFixed(2), newBalance: 0 });
-        // saving event to db
-        event.save(function(err, event) {
-
-        if (err) return console.error(err);
-
-        helpers.recalcBalance(req.params.id);
-
-        console.log('Event added')
-
-        res.redirect(`/user/client/${req.params.id}`)
-
-    })})
-
-})
-
-router.get('/deleteevent/:id/:eventid', connectEnsureLogin.ensureLoggedIn(), function (req, res) {
-
-
-    Event.findByIdAndDelete(req.params.eventid, function (err, event) {
-
-        if (err) return console.error(err);
-
-        Client.findOneAndUpdate({ _id: req.params.id }, { $inc: { balance: - parseInt(event.amount.toString()) }}, function(err, result) {
-                
-            if (err) console.error(err);
-
-            else console.log(result);
-        })
-
-    })
-
-    res.redirect(`/user/client/${req.params.id}`);
-
-})
-
-router.get('/client/event/:eventid', connectEnsureLogin.ensureLoggedIn(), async function (req, res) {
-
-    Event.findOne({_id: req.params.eventid}, function(err, event) {
-        
-        if (err) return console.error(err);
-        
-        console.log(event)
-        res.render('eventpage', { event: event });
-    });
- 
-
-})
-
-router.post('/client/event/:eventid', async function(req, res) {
-   
-        let hrs = parseFloat(req.body.hours)
-        let mins = parseFloat(req.body.minutes)
-        let duration = hrs + (mins / 10)
-        let rate = req.body.rate
-        let amount = req.body.type != "Retainer" ? -(duration * rate): req.body.amount;
-        let detail = req.body.detail
-        let clientID = ''
-        
-        Event.findOneAndUpdate({ _id: req.params.eventid }, { type: req.body.type, duration: duration, rate: rate, amount: amount, detail: detail }, function (err, docs) {
-
-        if (err) return console.error(err)
-
-        clientID = docs.clientID
-        //find all events that belong to this client so the new balance can be calculated
-        helpers.recalcBalance(clientID);
-
-        res.redirect(`/user/client/${clientID}`)
-    })
-})
-
-router.post('/client/:id/makestatement/:fname/:lname', (req, res) => {
-
-    const start = req.body.startdate;
-    const end = req.body.enddate;
-    let userArg;
-    
-    let userInfo = {  
-
-        clientname: req.params.fname + " " + req.params.lname,
-        billingAdd: req.user.street ? req.user.street + ", " + req.user.city + ", " + req.user.state + " " + req.user.zip : "",
-        mailingAdd: "", // this isnt handled client side yet 
-        phone: req.user.phone
-
-    };
-
-    userArg = JSON.stringify(userInfo);
-    eventsArg = JSON.parse(req.body.events);
-    // sort the events and filter out the ones we dont need
-    eventsArg.events.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
-    eventsArg.events = eventsArg.events.filter((e) => new Date(e.date).getTime() >= new Date(start).getTime() && new Date(e.date).getTime() <= new Date(end).getTime());
-
-    console.log(userArg);
-    console.log(eventsArg.events);
-    let options = {
-        mode: "text",
-        args: [start, end, userArg, JSON.stringify(eventsArg)]
-    }
-
-    PythonShell.run("Python/src/core/main.py", options, (err, result) => {
-
-        if (err) return console.error(err)
-
-        console.log("++++++++++++++++++++++++++++++++++ \n" + result)
-        
-        console.log(result)
-
-        res.redirect(`/user/client/${req.params.id}/makestatement/download/${userInfo.clientname}/${start}/${end}`);
-
-    })
-
-
-    //res.redirect(`/user/client/${req.params.id}/makestatement/download/${clientname}/${start}/${end}`);
-
-})
-
-router.get('/client/:id/makestatement/download/:clientname/:start/:end', function (req, res) {
-
-
-    res.download(`public/invoices/${req.params.clientname}.pdf`, `${req.params.clientname} ${req.params.start}-${req.params.end}.pdf`, function (err) {
-
-        if (err) return console.error(err);
-
-        
-        // delete the pdf from the server after download
-        fs.unlink(`public/invoices/${req.params.clientname}.pdf`, function (err) {
-            
-            if (err) return console.error(err)
-
-        });
-    })
-
-})
 
 module.exports = router;
