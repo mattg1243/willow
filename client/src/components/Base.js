79493b9da566967a4e5dec0340f411045b42f593
Matt Gallucci <mattgallucci97@gmail.com>
2021-12-27 23:10:38 -0800

    basic client table component added

--- client/src/components/Base.js
+++ client/src/components/Base.js
@@ -3,23 +3,21 @@ import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom'
 import { Input, Container, VStack, HStack, Button } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from "axios"
 import ClientTable from "./ClientTable"
 
-export default function Base() {
+export default function Base(props) {
     
-    const clients = useSelector((state) => {return state.clients});
+    const clients = useSelector((state) => {return state.user.clients});
     
     useEffect(() => {
         console.log(clients);
     })
     return (
         <>
-        <Container>
-            <h1>Clients</h1>
-            {clients.map(client => (<h3>{client.fname}</h3>))}
-        </Container>
-        
+            <h1>Base Component</h1>
+            <ClientTable />
         </>
     )
 }
