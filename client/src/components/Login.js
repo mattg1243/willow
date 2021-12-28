fa4a05fb8b407cb9c787b3cb6c2308a46c2f3e8f
Matt Gallucci <mattgallucci97@gmail.com>
2021-12-27 16:33:17 -0800

    good response on login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState } from "react";
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Input, Container, VStack, HStack, Button } from '@chakra-ui/react';
 import { useDispatch } from 'react-redux';
 import axios from "axios"
 import { loginAction } from "../actions"
+import { clientsAction } from '../actions';
 
 
 export default function Login() {
@@ -18,13 +19,14 @@ const navigate = useNavigate();
 const loginUser = async (username, password) => {
     const configObject = {
         method: "POST",
-        url: "http://localhost:8080/login",
+        url: "http://localhost:3000/login",
         data: { username: username, password: password },
     };
     const response = await axios(configObject).catch(err => {console.error(err);})
     console.log(response.data)
     if (response.data) {
-        dispatch(loginAction(response.data));
+        dispatch(loginAction(response.data.user))
+        dispatch(clientsAction(response.data.clients));
         navigate('/dashboard');
     } else {
         return <h1>err</h1>
@@ -32,11 +34,11 @@ const loginUser = async (username, password) => {
 }
 
 return (
-    <Container className="loginCont">
-        <VStack style={{height: '50%', marginTop: '20%', padding: '1rem'}}>
+    <Container style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <VStack className="loginCont" >
             <h3 className="willowCursive">Willow</h3>
-            <Input placeholder="Email" type="email" variant='flushed' focusBorderColor="#03b126" onChange={(e) => {setUsername(e.target.value)}}/>
-            <Input placeholder="Password" type="password" variant='flushed' focusBorderColor="#03b126" onChange={(e) => {setPassword(e.target.value)}}/>
+            <Input className="textInput" placeholder="Email" type="email" variant='flushed' focusBorderColor="#03b126" onChange={(e) => {setUsername(e.target.value)}}/>
+            <Input className="textInput" placeholder="Password" type="password" variant='flushed' focusBorderColor="#03b126" onChange={(e) => {setPassword(e.target.value)}}/>
             <HStack>
                 <Button background="#03b126" color="#fff" onClick={() => {loginUser(username, password)}}>Login</Button>
                 <Button background="#63326E" color="#fff">Register</Button>
