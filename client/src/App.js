4c1c0f877edc9996597cd178625f87aadcf5b004
Matt Gallucci <mattgallucci97@gmail.com>
2021-12-28 00:23:19 -0800

    better routing from App component, styling changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import Login from './components/Login';
-import Dashboard from './components/Dashboard';
 import ClientPage from './components/ClientPage';
+import ClientTable from './components/ClientTable';
 import './style.css';
 import { Center, ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
+import Base from './components/Base';
 
-const store = createStore(rootReducer);
-
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 function App() {
   return (
@@ -20,7 +23,7 @@ function App() {
           <Router>
             <Routes>
               <Route path='/' element={<Login />} />
-              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/dashboard' element={<Base />} />
             </Routes>
           </Router>
         </Provider>
