c403e08e092176a947795c7be40a71f4166038d3
Matt Gallucci <mattgallucci97@gmail.com>
2021-12-31 01:16:25 -0800

    changed post route for adding a client

--- models/client-schema.js
+++ models/client-schema.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const ClientSchema = new Schema({
 
-    ownerID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    ownerID: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
     fname: {type: String, required: true, maxLength: 100},
     lname: {type: String, required: true, maxLength: 100},
     phonenumber: {type: String, required: false, maxLength: 100},
-    sessions: {type: Array, required: false}, // this needs to be changed to an array of IDs and refactored to "events"
+    sessions: [{type: mongoose.Schema.Types.ObjectId, ref: 'events'}], // this needs to be changed to an array of IDs and refactored to "events"
     balance: {type: mongoose.Schema.Types.Decimal128}
 
 })
