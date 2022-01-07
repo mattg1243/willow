de6b30e2c53bd8d9e45e0bd68334586462144c2e
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-06 23:58:52 -0800

    quick statement drawer component completed

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -7,7 +7,7 @@ import {
     MenuDivider,
     IconButton,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useColorMode } from '@chakra-ui/color-mode';
 import { IoSettingsSharp } from 'react-icons/io5';
 import { GiHamburgerMenu } from 'react-icons/gi';
@@ -15,38 +15,45 @@ import { BiLogOut } from 'react-icons/bi';
 import { FaFileInvoiceDollar } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
+import QuickStatement from './QuickStatement';
 
-function HamburgerMenu() {
+export default function HamburgerMenu() {
+    
+    const [statementDrawerOpen, setStatementDrawerOpen] = useState(false);
+    
     const { colorMode } = useColorMode()
     const isDark = colorMode === 'dark'
 
     const navigate = useNavigate();
 
+    useEffect(() => {console.log(statementDrawerOpen)})
+
     return(
+        <>
         <Menu>
-          <MenuButton 
-          as={IconButton}
-          aria-label='Menu'
-          icon={<GiHamburgerMenu size={25} />}
-          variant='flat'
-        />
-          <MenuList >
-            <MenuItem icon={<BsFillPersonLinesFill color={isDark? "white" : "black"} />} color={isDark? "white" : "black"} onClick={() => { navigate('/clients') }} >
-                Clients
-            </MenuItem>
-            <MenuItem icon={<FaFileInvoiceDollar color={isDark? "white" : "black"} />} color={isDark? "white" : "black"}>
-                Quick Statement
-            </MenuItem>
-            <MenuDivider />
-            <MenuItem icon={<IoSettingsSharp color={isDark? "white" : "black"}/>} color={isDark? "white" : "black"}>
-                Settings
-            </MenuItem>
-            <MenuItem icon={<BiLogOut color="#EC4E20" />} color={isDark? "white" : "black"}>
-                Logout
-            </MenuItem>
-          </MenuList>
+            <MenuButton 
+            as={IconButton}
+            aria-label='Menu'
+            icon={<GiHamburgerMenu size={25} />}
+            variant='flat'
+                />
+            <MenuList >
+                <MenuItem icon={<BsFillPersonLinesFill color={isDark? "white" : "black"} />} color={isDark? "white" : "black"} onClick={() => { navigate('/clients') }} >
+                    Clients
+                </MenuItem>
+                <MenuItem icon={<FaFileInvoiceDollar color={isDark? "white" : "black"} />} color={isDark? "white" : "black"} onClick={() => { setStatementDrawerOpen(true) }}>
+                    Quick Statement
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem icon={<IoSettingsSharp color={isDark? "white" : "black"}/>} color={isDark? "white" : "black"}>
+                    Settings
+                </MenuItem>
+                <MenuItem icon={<BiLogOut color="#EC4E20" />} color={isDark? "white" : "black"}>
+                    Logout
+                </MenuItem>
+            </MenuList>
         </Menu>
+        <QuickStatement statementDrawerOpen={statementDrawerOpen} setStatementDrawerOpen={setStatementDrawerOpen}/>
+        </>
     )
 }
-
-export default HamburgerMenu;
