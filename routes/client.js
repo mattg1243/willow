d89aabbc5507910e9ac7418b5b23094dfff69bb0
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-06 18:32:29 -0800

    removed middleware from individual routes

--- routes/client.js
+++ routes/client.js
@@ -5,19 +5,19 @@ const helpers = require('./helpers/helpers');
 
 
 // render the client page and get all events
-router.get("/:id", connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.renderClientPage(req, res) });
+router.get("/:id", (req, res) => { handlers.renderClientPage(req, res) });
 // add new event to clients record
 router.post('/:id/addevent', (req, res) => { handlers.addEvent(req, res); });
 // get a specific event and render the event page
-router.get('/event/:eventid', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.renderEventPage(req, res); });
+router.get('/event/:eventid', (req, res) => { handlers.renderEventPage(req, res); });
 // update an event
-router.post('/event/:eventid', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.updateEvent(req, res); });
+router.post('/event/:eventid', (req, res) => { handlers.updateEvent(req, res); });
 // delete an event
-router.get('/deleteevent/:id/:eventid', connectEnsureLogin.ensureLoggedIn(), function (req, res) { handlers.deleteEvent(req, res) });
+router.post('/deleteevent', (req, res) => { handlers.deleteEvent(req, res) });
 // generate a statment
-router.post('/:id/makestatement/:fname/:lname', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.makeStatement(req, res); })
+router.post('/:id/makestatement/:fname/:lname', (req, res) => { handlers.makeStatement(req, res); })
 // download the statement
-router.get('/:id/makestatement/download/:clientname/:start/:end', connectEnsureLogin.ensureLoggedIn(), (req, res) => { handlers.downloadStatement(req, res); })
+router.get('/:id/makestatement/download/:clientname/:start/:end', (req, res) => { handlers.downloadStatement(req, res); })
 
 
 module.exports = router;
