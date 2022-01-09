b465186300310cee38cc7a5f4ad8dc624f7f9a2d
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-09 13:28:28 -0800

    added payment info attribute to user schema and associated server logic/front end input fields

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,9 +5,11 @@ import {
     HStack, 
     Button, 
     FormLabel,
-    Heading
+    Heading,
+    Tooltip,
 } from '@chakra-ui/react';
 import { useColorMode } from '@chakra-ui/color-mode';
+import { InfoIcon } from '@chakra-ui/icons'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginAction, getClients } from '../actions';
@@ -24,6 +26,7 @@ export default function Profile() {
     const [zip, setZip] = useState(`${user.zip}`);
     const [state, setState] = useState(`${user.state}`);
     const [phone, setPhone] = useState(`${user.phone}`)
+    const [paymentInfo, setPaymentInfo] = useState(`${user.paymentInfo}`)
 
     const { colorMode } = useColorMode();
     const isDark = colorMode === 'dark';
@@ -39,6 +42,7 @@ export default function Profile() {
             zip: zip,
             phone: phone,
             state: state,
+            paymentInfo: paymentInfo,
         })
         .then(response => {console.log(response); dispatch(loginAction(response.data)); } )
         .catch(err => {console.error(err);})
@@ -47,7 +51,7 @@ export default function Profile() {
     return (
         <>
         <Header />
-            <VStack style={{height: '100%', width: '70%'}} spacing={5}>
+            <VStack style={{height: '100%', width: '60%'}} spacing={5}>
                 <Heading style={{fontFamily: '"Quicksand", sans-serif', fontSize: '3rem', paddingBottom: '2rem'}}>Profile</Heading>
                 <FormLabel>Name for Header</FormLabel>
                 <Input type="text" placeholder={user.nameForHeader} onChange={(e) => { setName(e.target.value) }}/>
@@ -67,6 +71,10 @@ export default function Profile() {
                 </HStack>
                 <FormLabel>Phone Number</FormLabel>
                 <Input type="text" placeholder={user.phone} onChange={(e) => { setPhone(e.target.value) }}/>
+                <Tooltip label="This will tell your clients how you'd like to receive payment. It will show on the bottom of statements, but is not required.">
+                    <FormLabel>Payment Info <InfoIcon style={{color: 'grey'}}/></FormLabel>
+                </Tooltip>
+                <Input type="text" placeholder={user.paymentInfo ? user.paymentInfo: null} onChange={(e) => { setPaymentInfo(e.target.value) }}/>
                 <HStack spacing={12} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', padding: '3rem'}} width="100%">
                     <Button style={{backgroundColor: isDark? "#63326E" : '#03b126', color: 'white'}} onClick={() => { updateInfo(); navigate('/clients'); }}>Save</Button>
                     <Button style={{backgroundColor: isDark? "#EC4E20" : '#58A4B0', color: 'white'}}>Cancel</Button>
