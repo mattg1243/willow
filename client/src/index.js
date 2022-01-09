b1e468f1be91ce83ed5bf399b0b1f5a321b47d46
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-09 14:21:24 -0800

    new mobile breakpoints for table

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,7 @@ const breakpoints = {
   mobile: 320,
   mobileLandscape: 480,
   tablet: 768,
-  tabletLandscape: 1024,
-  desktop: 1200,
+  desktop: 1024,
   desktopLarge: 1500,
   desktopWide: 1920,
 }
