5491e89837df6d4f53e28594fb080e969867b261
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-08 16:34:20 -0800

    added email field in client schema

--- models/client-schema.js
+++ models/client-schema.js
@@ -7,6 +7,7 @@ const ClientSchema = new Schema({
     fname: {type: String, required: true, maxLength: 100},
     lname: {type: String, required: true, maxLength: 100},
     phonenumber: {type: String, required: false, maxLength: 100},
+    email: {type: String, required: false, maxLength: 100},
     sessions: [{type: mongoose.Schema.Types.ObjectId, ref: 'events'}], // this needs to be changed to an array of IDs and refactored to "events"
     balance: {type: mongoose.Schema.Types.Decimal128}
 
