1779e62e3304f33ddb5f101f7d8f6ff6af22d676
Matt Gallucci <mattgallucci97@gmail.com>
2022-02-27 14:48:56 -0800

    added support for client billing rate when creating a new client

--- client/src/components/AddClientForm.js
+++ client/src/components/AddClientForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Input, VStack, Stack, Button, Divider  } from '@chakra-ui/react';
+import { Input, VStack, Button, Divider, FormLabel } from '@chakra-ui/react';
 import { useColorMode } from '@chakra-ui/color-mode';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
@@ -11,6 +11,7 @@ export default function AddClientForm(props) {
     const [lname, setLname] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+    const [rate, setRate] = useState(0);
 
     const { colorMode } = useColorMode()
     const isDark = colorMode === 'dark'
@@ -22,11 +23,12 @@ export default function AddClientForm(props) {
 
     const addClient = async () => {
         
-        const response = await axios.post('/user/newclient', {
+        await axios.post('/user/newclient', {
             fname: fname,
             lname: lname,
             email: email,
             phonenumber: phone,
+            rate: rate,
             user: user.id,
             token: token,
         }).then(response => {
@@ -42,15 +44,18 @@ export default function AddClientForm(props) {
     return (
         <>
             <VStack >
+                <FormLabel>First Name</FormLabel>
+                <Input onChange={(e) => { setFname(e.target.value) }} />
+                <FormLabel>Last Name</FormLabel>
+                <Input onChange={(e) => { setLname(e.target.value) }} />
+                <FormLabel>Email</FormLabel>
+                <Input onChange={(e) => { setEmail(e.target.value) }} />
+                <FormLabel>Phone Number</FormLabel>
+                <Input onChange={(e) => { setPhone(e.target.value) }} />
+                <FormLabel>Billing Rate</FormLabel>
+                <Input onChange={(e) => { setRate(e.target.value) }} />
                 <Divider />
-                    <Stack spacing={7} >
-                        <Input placeholder="First Name" onChange={(e) => { setFname(e.target.value) }} />
-                        <Input placeholder="Last Name" onChange={(e) => { setLname(e.target.value) }} />
-                        <Input placeholder="Email" onChange={(e) => { setEmail(e.target.value) }} />
-                        <Input placeholder="Phone Number" onChange={(e) => { setPhone(e.target.value) }} />
-                    </Stack>
-                <Divider />
-                <Button style={{backgroundColor: isDark? "#63326E" : '#03b126', color: 'white'}} onClick={() => { addClient(); }}>Save</Button>
+                <Button style={{backgroundColor: isDark? "#63326E" : '#03b126', color: 'white', marginTop: '1rem'}} onClick={() => { addClient(); }}>Save</Button>
             </VStack>
             
         </>
