4503273a117d7a090ec1866f01ec0bf44a5eb45b
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-24 15:15:56 -0700

    inserted validation middleware into request handling for login and some user routes

--- routes/user.js
+++ routes/user.js
@@ -4,18 +4,48 @@ const helpers = require('./helpers/helpers');
 const validators = require('./validators/userValidators');
 
 // register new user to DB
-router.post('/register/newuser', validators.registerUserValidator, (req, res) => { handlers.registerUser(req, res) })
+router.post('/register/newuser', 
+  validators.registerUserValidator,
+  (req, res) => { handlers.registerUser(req, res) }
+);
 // update users info from preferences page
-router.post("/updateinfo", helpers.verifyJWT, (req, res) => { handlers.updateUserInfo(req, res) }) // p
+router.post("/updateinfo", 
+  validators.updateUserInfoValidator,
+  helpers.verifyJWT, 
+  (req, res) => { handlers.updateUserInfo(req, res) }
+);
 // add a new client
-router.post('/newclient', helpers.verifyJWT, (req, res) => { handlers.addNewClient(req, res) }); // p
+router.post('/newclient', 
+  validators.newClientValidator,
+  validators.validate,
+  helpers.verifyJWT, 
+  (req, res) => { handlers.addNewClient(req, res) }
+);
 // delete a client and all associated events
-router.post('/deleteclient', helpers.verifyJWT, (req, res) => { handlers.deleteClient(req, res) }); // p
+router.post('/deleteclient', 
+  validators.deleteClientValidator, 
+  validators.validate, 
+  helpers.verifyJWT, 
+  (req, res) => { handlers.deleteClient(req, res) }
+);
 // update a clients information
-router.post('/updateclient', helpers.verifyJWT, (req, res) => { handlers.updateClientInfo(req, res) });
+router.post('/updateclient', 
+  validators.udpateClientValidator,
+  validators.validate,
+  helpers.verifyJWT, 
+  (req, res) => { handlers.updateClientInfo(req, res) }
+);
 // reset password
-router.post('/resetpassword', (req, res) => { handlers.resetPassword(req, res) });
+router.post('/resetpassword', 
+  validators.resetPasswordValidator,
+  validators.validate,
+  (req, res) => { handlers.resetPassword(req, res) }
+);
 // save new password
-router.post('/changepassword', (req, res) => { handlers.changePassword(req, res) });
+router.post('/changepassword',
+  validators.changePasswordValidator,
+  validators.validate,
+  (req, res) => { handlers.changePassword(req, res) }
+);
 
 module.exports = router;
