cb2995e6e042a1942f817d1330e16caa5b6f05a5
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-24 15:14:57 -0700

    more validation rules set for register and update user routes

--- routes/validators/userValidators.js
+++ routes/validators/userValidators.js
@@ -1,6 +1,14 @@
-const { body, validationResult } = require('express-validator');
+const { body, check, validationResult } = require('express-validator');
 
-exports.registerUserValidator = [
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json(errors.array())
+  }
+  next();
+}
+
+const registerUserValidator = [
   // validate and sanitize 
     // username field
     body('username', 'No username entered.')
@@ -8,135 +16,126 @@ exports.registerUserValidator = [
     .isLength({ min: 7, max: 30 })
     .withMessage('Please enter a username that is between 7 and 30 characters long.')
     .escape(),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
     // password and confirmation fields
     body('passwordConfirm')
     .trim()
     .isLength({ min: 7, max: 30 })
     .withMessage('Please enter a password that is between 7 and 30 characters long.')
-    .custom(async (passwordConfirm) => {
-        const password = req.body.password;
-        // throw error if passwords dont match
-        if (password != passwordConfirm) {
-            throw new Error('Passwords do not match')
-        }
-    }),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
+    .custom((passwordConfirm, { req }) => passwordConfirm == req.body.password)
+    .withMessage('Passwords do not match'),
     // other fields that just need basic validation
     body('fname').trim().escape().isAlpha()
     .withMessage('Only alphabetical characters are allowed in the name field.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).send(errors.array());
-      }
-      next();
-    },
 
     body('lname').trim().escape().isAlpha()
     .withMessage('Only alphabetical characters are allowed in the name field.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
 
     body('nameForHeader', 'Please enter a name to appear on your statements.')
     .trim().escape(),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
 
     body('email').trim().escape().isEmail()
     .withMessage('Invalid email address.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
 
     body('phone').trim().escape().isNumeric()
     .withMessage('Invalid phone number.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
-    
+
     body('city').trim().escape().isAlpha()
     .withMessage('Only alphabetical characters are allowed in the city field.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
 
     body('state').trim().escape().isAlpha().isLength({ min: 2, max: 2 })
     .withMessage('Please enter your states two letter code (i.e. CA, NV)'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
 
     body('zip').trim().escape().isPostalCode()
     .withMessage('Zip code should be all numbers.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(errors.array());
-      }
-      next();
-    },
+
+]
+
+const loginUserValidator = [
   
+  body('username', 'Username contains invalid characters.').trim().escape(),
+
+  body('password', 'Password contains invalid characters.').trim().escape(),
+
 ]
 
-exports.loginUserValidator = [
+const updateUserInfoValidator = [
+  // make sure user id is in a valid format
+  body('user', 'Invalid characters in the user ID. What are you trying to do here?').trim().escape(),
+  // check formatting for the optional text fields
+  body('nameForHeader', 'Check "Name for Header for invalid characters').trim().escape()
+  .isLength({ min: 2, max: 100})
+  .withMessage('Name for Header exceeds 100 characters'),
+
+  body('street', 'Check "Street Adress" for invalid characters').trim().escape()
+  .isLength({ min: 2, max: 50})
+  .withMessage('Street address may not exceed 50 characters'),
   
+  body('city', 'Check "Street Adress" for invalid characters').trim().escape()
+  .isLength({ min: 2, max: 50})
+  .withMessage('Street address may not exceed 50 characters'),
+
+  body('state', 'Check "State" for invalid characters').trim().escape().isAlpha().isLength({ min: 2, max: 2 })
+  .withMessage('Please enter your states two letter code (i.e. CA, NV)'),
+
+  body('zip', 'Check "Zip Code" for invalid characters').trim().escape().isNumeric()
+  .withMessage('Zip code should be all numbers.'),
+  
+  body('email', 'Check "Email" for invalid characters').trim().escape().isEmail()
+  .withMessage('Invalid email address.')
+  .isLength({ min: 1, max: 150}),
+    
+  body('phone', 'Check "Phone" for invalid characters').trim().escape().isNumeric().isLength({ min: 1, max: 150})
+  .withMessage('Invalid phone number'),
+
+  body('paymentInfo', 'Check "Payment Info" for invalid characters').trim().escape()
+  .isLength({ min: 2, max: 80})
+  .withMessage('Payment Info field exceeds 80 charactes'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json(errors.array())
+    }
+    next();
+  }
+ 
 ]
 
-exports.updateUserInfoValidator = [
-
+const newClientValidator = [
+  // trim + escape all fields
+  check().trim().escape(),
 ]
 
-exports.newClientValidator = [
-
+const deleteClientValidator = [
+  // trim + escape all fields
+  check().trim().escape(),
 ]
 
-exports.udpateClientValidator = [
-
+const udpateClientValidator = [
+  // trim + escape all fields
+  check().trim().escape(),
 ]
 
-exports.resetPasswordValidator = [
-
+const resetPasswordValidator = [
+  // trim + escape all fields
+  check().trim().escape(),
 ]
 
-exports.changePasswordValidator = [
-
+const changePasswordValidator = [
+  // trim + escape all fields
+  check().trim().escape(),
 ]
+
+module.exports = {
+  
+  validate,
+  registerUserValidator,
+  loginUserValidator,
+  updateUserInfoValidator,
+  newClientValidator,
+  deleteClientValidator,
+  udpateClientValidator,
+  resetPasswordValidator,
+  changePasswordValidator
+
+}
