3881c9ad2db50cf102a0d149f39d055c219fbfd5
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-26 18:49:08 -0700

    more minimal design for BadInputAlert component

--- client/src/components/BadInputAlert.js
+++ client/src/components/BadInputAlert.js
@@ -11,11 +11,9 @@ export default function BadInputAlert(props) {
   return (
     <Alert status='error'>
       <AlertIcon />
-      <AlertTitle mr={2}>Input Error</AlertTitle>
       <AlertDescription>
         {props.errMsg}
       </AlertDescription>
-      <CloseButton position='absolute' right='8px' top='8px'/>
     </Alert>
   )
 }
