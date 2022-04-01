650b91871d293fd723c3806964a3f559358817a2
Matt Gallucci <mattgallucci97@gmail.com>
2022-04-01 11:18:45 -0700

    validators completed for client routes

--- routes/client.js
+++ routes/client.js
@@ -1,23 +1,33 @@
 const router = require('express').Router();
-const connectEnsureLogin = require('connect-ensure-login');
 const handlers = require('./handlers/clientHandlers');
 const helpers = require('./helpers/helpers');
+const validators = require('./validators/clientValidators');
 
-
-// render the client page and get all events
-router.get("/:id", (req, res) => { handlers.renderClientPage(req, res) });
+// need to add jwt verification for these routes
 // add new event to clients record
-router.post('/:id/addevent', (req, res) => { handlers.addEvent(req, res); });
-// get a specific event and render the event page
-router.get('/event/:eventid', (req, res) => { handlers.renderEventPage(req, res); });
+router.post('/:id/addevent', 
+  validators.addEventValidator,
+  validators.validate,
+  (req, res) => { handlers.addEvent(req, res); 
+});
 // update an event
-router.post('/event/:eventid', (req, res) => { handlers.updateEvent(req, res); });
+router.post('/event/:eventid', 
+  validators.addEventValidator,
+  validators.validate,
+  (req, res) => { handlers.updateEvent(req, res); 
+});
 // delete an event
-router.post('/deleteevent', (req, res) => { handlers.deleteEvent(req, res) });
-// generate a statment
-router.post('/makestatement', (req, res) => { handlers.makeStatement(req, res); })
-// download the statement
-router.get('/:id/makestatement/download/:clientname/:start/:end', (req, res) => { handlers.downloadStatement(req, res); })
+router.post('/deleteevent', 
+  validators.deleteEventValidator,
+  validators.validate,
+  (req, res) => { handlers.deleteEvent(req, res) 
+});
+// generate and download statment
+router.post('/makestatement', 
+  validators.makeStatementValidator,
+  validators.validate,
+  (req, res) => { handlers.makeStatement(req, res); 
+})
 
 
 module.exports = router;
