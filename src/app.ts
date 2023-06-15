0ab7dfc6daed6841ca71be9958299061fddcbe12
Matt Gallucci <mattgallucci97@gmail.com>
2023-06-15 14:15:53 -0700

    changed /events base route to /event, formatting

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,7 @@ import cors from 'cors';
 import User from './models/user-model';
 // import User from './models/user-model';
 
-
-require('dotenv').config({path: path.resolve(__dirname, '../.env')});
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 // check for production or dev env
 
 import loginRouter from './routes/login';
@@ -36,7 +35,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 // render from React build
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // set view engine to satisify error codes
 app.set('view engine', 'jade');
@@ -45,39 +44,34 @@ let browser: puppeteer.Browser;
 let page: puppeteer.Page;
 (async () => {
   browser = await puppeteer.launch({
-  headless: true,
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   page = await browser.newPage();
 })();
 export { page };
 // middleware
-app.use(helmet.contentSecurityPolicy({
-  useDefaults: false,
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: [
-      "'self'",
-      "https://code.jquery.com/jquery-3.2.1.slim.min.js",
-      'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
-      "https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js",
-    ],
-    styleSrc: [
-      "'self'",
-      "'unsafe-inline'",
-      "*",
-    ],
-    fontSrc: [
-      "'self'",
-      "*",
-    ]
-  }
-}))
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js',
+      ],
+      styleSrc: ["'self'", "'unsafe-inline'", '*'],
+      fontSrc: ["'self'", '*'],
+    },
+  })
+);
 app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use('/scripts', express.static(path.join(__dirname, '../public/javascripts')))
-app.use('/images', express.static(path.join(__dirname, '../public/images')))
-app.use('/invoices', express.static(path.join(__dirname, '../public/invoices')))
+app.use('/scripts', express.static(path.join(__dirname, '../public/javascripts')));
+app.use('/images', express.static(path.join(__dirname, '../public/images')));
+app.use('/invoices', express.static(path.join(__dirname, '../public/invoices')));
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
@@ -89,7 +83,7 @@ app.use(express.json());
 //   secure: true
 // }));
 app.use(flash());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
@@ -101,14 +95,12 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Setting local variables for the front end
-app.use(function(req, res, next) {
-
+app.use(function (req, res, next) {
   res.locals.loggedIn = req.isAuthenticated();
   res.locals.eventType = 'none';
 
   next();
-  
-})
+});
 
 // Base routes
 // app.use('/', indexRouter);
@@ -117,19 +109,19 @@ app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/api', verifyJWT, apiRouter);
 app.use('/client', verifyJWT, clientRouter);
-app.use('/events', eventsRouter);
+app.use('/event', eventsRouter);
 // rendering from react build
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-})
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -139,6 +131,5 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(port);
-
 
 export default app;
